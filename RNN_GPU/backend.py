120fd57179a8ba058c39996b0de9283685d9b384
poolen <poolen@unlv.nevada.edu>
2019-10-03 12:07:02 -0700

    RNN with GPU acceleration

---
+++ RNN_GPU/backend.py
@@ -0,0 +1,9 @@
+from keras import backend as K
+import os
+
+def set_keras_backend(backend):
+
+    if K.backend() != backend:
+        os.environ['KERAS_BACKEND'] = backend
+        K.reload(K)
+        assert K.backend() == backend
---
+++ RNN_GPU/data_path.py
@@ -0,0 +1,4 @@
+
+
+training_set_path = '/home/terra/Senior Project/seniorprojectfall2019team7/RNN_GPU/Google_Stock_Price_Train.csv'
+test_set_path = '/home/terra/Senior Project/seniorprojectfall2019team7/RNN_GPU/Google_Stock_Price_Test.csv'
