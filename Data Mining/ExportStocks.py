3ec955b42b352e43574dbedeac607a83197136ce
Kevin Heward <kbheward1@gmail.com>
2019-10-17 20:43:15 -0700

    Altered ExportStocks.py to only take in Nasdaq Stocks and added all Nasdaq stocks to repository

--- Data Mining/ExportStocks.py
+++ Data Mining/ExportStocks.py
@@ -3,39 +3,140 @@ import time
 import os
 import sys
 
-stock_sym = ["AAPL", "GOOGL", "MSFT", "AMZN", "FB", "BRK-A" , "BRK-B" , "BABA", "JNJ", "JPM", "XOM", "BAC",
-                "WMT", "WFC", "RDS-A", "RDS-B", "V", "PG", "BUD", "T", "CVX", "UNH", "PFE", "RHHBY", "CHL",
-                "HD", "INTC", "VZ", "ORCL", "NVS"]
+stock_sym = {
+"ATVI":	"Activision Blizzard Inc",
+"ADBE":	"Adobe Inc.",
+"AMD":	"Advanced Micro Devices Inc",
+"ALGN":	"Align Technology Inc",
+"ALXN":	"Alexion Pharmaceuticals Inc",
+"AMZN":	"Amazon.com Inc",
+"AMGN":	"Amgen Inc",
+"AAL":	"American Airlines Group Inc",
+"ADI":	"Analog Devices Inc",
+"AAPL":	"Apple Inc",
+"AMAT":	"Applied Materials Inc",
+"ASML":	"ASML Holding NV",
+"ADSK":	"Autodesk Inc",
+"ADP":	"Automatic Data Processing Inc",
+"AVGO":	"Broadcom Inc",
+"BIDU":	"Baidu Inc",
+"BIIB":	"Biogen Inc",
+"BMRN":	"Biomarin Pharmaceutical Inc",
+"CDNS":	"Cadence Design Systems Inc",
+"CELG":	"Celgene Corp",
+"CERN":	"Cerner Corp",
+"CHKP":	"Check Point Software Technologies Ltd",
+"CHTR":	"Charter Communications Inc",
+"CTRP":	"Ctrip.Com International Ltd",
+"CTAS":	"Cintas Corp",
+"CSCO":	"Cisco Systems Inc",
+"CTXS":	"Citrix Systems Inc",
+"CMCSA":	"Comcast Corp",
+"COST":	"Costco Wholesale Corp",
+"CSX":	"CSX Corp",
+"CTSH":	"Cognizant Technology Solutions Corp",
+"DLTR":	"Dollar Tree Inc",
+"EA":	"Electronic Arts",
+"EBAY":	"eBay Inc",
+"EXPE":	"Expedia Group Inc",
+"FAST":	"Fastenal Co",
+"FB":	"Facebook",
+"FISV":	"Fiserv Inc",
+"GILD":	"Gilead Sciences Inc",
+"GOOG":	"Alphabet Class C",
+"GOOGL":	"Alphabet Class A",
+"HAS":	"Hasbro Inc",
+"HSIC":	"Henry Schein Inc",
+"ILMN":	"Illumina Inc",
+"INCY":	"Incyte Corp",
+"INTC":	"Intel Corp",
+"INTU":	"Intuit Inc",
+"ISRG":	"Intuitive Surgical Inc",
+"IDXX":	"IDEXX Laboratories Inc",
+"JBHT":	"J.B. Hunt Transport Services Inc",
+"JD":	"JD.com Inc",
+"KLAC":	"KLA Corp",
+"KHC":	"Kraft Heinz Co",
+"LRCX":	"Lam Research Corp",
+"LBTYA":	"Liberty Global PLC",
+"LBTYK":	"Liberty Global PLC",
+"LULU":	"Lululemon Athletica Inc",
+"MELI":	"Mercadolibre Inc",
+"MAR":	"Marriott International Inc",
+"MCHP":	"Microchip Technology Inc",
+"MDLZ":	"Mondelez International Inc",
+"MNST":	"Monster Beverage Corp",
+"MSFT":	"Microsoft Corp",
+"MU":	"Micron Technology Inc",
+"MXIM":	"Maxim Integrated Products Inc",
+"MYL":	"Mylan NV",
+"NTAP":	"NetApp Inc",
+"NFLX":	"Netflix Inc",
+"NTES":	"NetEase Inc",
+"NVDA":	"NVIDIA Corp",
+"NXPI":	"NXP Semiconductors NV",
+"ORLY":	"O'Reilly Automotive Inc",
+"PAYX":	"Paychex Inc",
+"PCAR":	"Paccar Inc",
+"BKNG":	"Booking Holdings Inc",
+"PYPL":	"PayPal Holdings Inc",
+"PEP":	"PepsiCo Inc.",
+"QCOM":	"Qualcomm Inc",
+"REGN":	"Regeneron Pharmaceuticals Inc",
+"ROST":	"Ross Stores Inc",
+"SIRI":	"Sirius XM Holdings Inc",
+"SWKS":	"Skyworks Solutions Inc",
+"SBUX":	"Starbucks Corp",
+"SYMC":	"Symantec Corp",
+"SNPS":	"Synopsys Inc",
+"TTWO":	"Take-Two Interactive Software Inc",
+"TSLA":	"Tesla Inc",
+"TXN":	"Texas Instruments Inc",
+"TMUS":	"T-Mobile US Inc",
+"ULTA":	"Ulta Beauty Inc",
+"UAL":	"United Airlines Holdings Inc",
+"VRSN":	"Verisign Inc",
+"VRSK":	"Verisk Analytics Inc",
+"VRTX":	"Vertex Pharmaceuticals Inc",
+"WBA":	"Walgreens Boots Alliance Inc",
+"WDC":	"Western Digital Corp",
+"WDAY":	"Workday Inc",
+"WYNN":	"Wynn Resorts Ltd",
+"XEL":	"Xcel Energy Inc",
+"XLNX":	"Xilinx Inc"
+}
 
-def downloadfiles(symbol):
+
+def downloadfiles(symbol, name):
 #Downloads the files from Alphavantage and stores them in a file called Stock_Data
 
     Front ="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
-    Back = "&apikey=FI97CUETTHW9CX8Q&datatype=csv"
-    urllib.request.urlretrieve(Front + symbol+ Back, "Stock_Data/" + symbol + ".csv")
+    Back = "&outputsize=full&apikey=FI97CUETTHW9CX8Q&datatype=csv"
+    urllib.request.urlretrieve(Front + symbol + Back, "Stock_Data/" + name.get(symbol) + ".csv")
 
 def create():
 #Checks to see if the Stock_Data File exists
 #if it does it moves on, if it doesn't then creates the file location
-	canCreate = "Stock_Data"
+    canCreate = "Stock_Data"
 
-	print ("Verifying required files and directories...")
-	if os.path.exists(canCreate) is False:
-		print (canCreate + "/ Doesn't exist, creating it...")
-		os.mkdir(canCreate)
+    print ("Verifying required files and directories...")
+    if os.path.exists(canCreate) is False:
+    	print (canCreate + "/ Doesn't exist, creating it...")
+    	os.mkdir(canCreate)
 
-	print ("Verification passed\n")
+    print ("Verification passed\n")
 
 
 if __name__ == "__main__":
 #calls function to create file location then stores files into Stock_Data
 #Only 5 files can be downloaded per minute, so time.sleep(13) pauses the next files
 #for 13 seconds. Currently 30 files are being downloaded per day which takes 6 minutes
-#to complete
-	create()
+#to complete 
 
-	for item in stock_sym:
-		downloadfiles(item)
-		time.sleep(13)
+    create()
 
-	sys.exit(0)
+    for item in stock_sym:
+        downloadfiles(item, stock_sym)
+        time.sleep(13)
+
+    sys.exit(0)
