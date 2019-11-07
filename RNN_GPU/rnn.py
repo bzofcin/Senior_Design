f346fc0c2e11ff03934017e9201c99127270a86b
Nick Poole <42500386+poolen1@users.noreply.github.com>
2019-11-07 12:44:07 +1100

    Accuracy calculator

--- RNN_GPU/rnn.py
+++ RNN_GPU/rnn.py
@@ -118,36 +118,43 @@ X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 predicted_stock_price = regressor.predict(X_test)
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
-# Calculating accuracy
-predicted_length = len(predicted_stock_price)
-# trend up == 1; trend down == 0; no change == 2
-predicted_trend = 0
-real_trend = 0
-total = 0
-accurate = 0
-percent_accurate = 0
-for i in range(1, predicted_length):
-    if predicted_stock_price[i] > predicted_stock_price[i-1]:
-        predicted_trend = 1
-    elif predicted_stock_price[i] < predicted_stock_price[i-1]:
-        predicted_trend = 0
-    elif predicted_stock_price[i] == predicted_stock_price[i-1]:
-        predicted_trend = 2
+# Output runtime
+print("--- %s seconds ---" % (time.time() - start_time))
 
-    if real_stock_price[i] > real_stock_price[i-1]:
-        real_trend = 1
-    elif real_stock_price[i] < real_stock_price[i-1]:
-        real_trend = 0
-    elif real_stock_price[i] == real_stock_price[i-1]:
-        real_trend = 2
+from accuracy_stats import calculate_accuracy
 
-    if predicted_trend == real_trend:
-        accurate += 1
+calculate_accuracy(predicted_stock_price, real_stock_price)
 
-    total += 1
-
-percent_accurate = (accurate/total) * 100
-print("Accuracy: " + str(percent_accurate) + "%")
+# # Calculating accuracy
+# predicted_length = len(predicted_stock_price)
+# # trend up == 1; trend down == 0; no change == 2
+# predicted_trend = 0
+# real_trend = 0
+# total = 0
+# accurate = 0
+# percent_accurate = 0
+# for i in range(1, predicted_length):
+#     if predicted_stock_price[i] > predicted_stock_price[i-1]:
+#         predicted_trend = 1
+#     elif predicted_stock_price[i] < predicted_stock_price[i-1]:
+#         predicted_trend = 0
+#     elif predicted_stock_price[i] == predicted_stock_price[i-1]:
+#         predicted_trend = 2
+#
+#     if real_stock_price[i] > real_stock_price[i-1]:
+#         real_trend = 1
+#     elif real_stock_price[i] < real_stock_price[i-1]:
+#         real_trend = 0
+#     elif real_stock_price[i] == real_stock_price[i-1]:
+#         real_trend = 2
+#
+#     if predicted_trend == real_trend:
+#         accurate += 1
+#
+#     total += 1
+#
+# percent_accurate = (accurate/total) * 100
+# print("Accuracy: " + str(percent_accurate) + "%")
 
 
 
@@ -159,6 +166,3 @@ plt.xlabel('Time')
 plt.ylabel('Google Stock Price')
 plt.legend()
 plt.show()
-
-# Output runtime
-print("--- %s seconds ---" % (time.time() - start_time))
