0c88630dbb2e385c2bbb174f9cf2829cc24be6cb
Nhung Barish <barisn1@unlv.nevada.edu>
2019-11-10 23:02:28 +0000

    Accounts Registration action update

--- User_Interface_Django/aistockpicker/accounts/views.py
+++ User_Interface_Django/aistockpicker/accounts/views.py
@@ -9,26 +9,27 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.sessions.models import Session
 from django.contrib.auth.decorators import login_required
-#from .models import Users
+from .models import myusers
 
 import hmac, base64, struct, hashlib, time
-
+import crypt, getpass, pwd
 import urllib
 import json
 import datetime
 
+from .models import users
 
 def login(request):
 
 
     if request.method == 'POST':
-        email = request.POST['email']
-        password= request.POST['password']
-        confirmPassword= request.POST['confirmpassword']
- #       a = Users()
- #       a.email = email
- #       a.password=password
- #       a.save()
+        user = myusers()
+        username = request.POST['username'].strip()
+        password = request.POST['password']
+        #password_hash = crypt.crypt(raw_password, 's0mRIdlKvIghsds131dsa9jlasdfj')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+           login(request, user)
 
     return render(request,'accounts/login.html',{})
 
@@ -39,16 +40,18 @@ def loggedIn(request):
 
 def register(request):
 
-    #if request.method == 'POST':
-
-        #username = form.cleaned_data.get('username')
-        #raw_password = form.cleaned_data.get('password1')
-        #user =
+    if request.method == 'POST':
+        user = myusers()
+        user.username = request.POST['username'].strip()
+        raw_password = request.POST['password']
+        password_hash = crypt.crypt(raw_password, 's0mRIdlKvIghsds131dsa9jlasdfj')
+        user.password = password_hash
+        user.save()
         #login(request, user)
-        #return redirect('home')
+        return redirect('login')
 
 
-    return render(request,'accounts/registration.html', {'authenticated': False })
+    return render(request,'accounts/registration.html', {'authenticated': False, 'pass': 'Empty' })
 
 
 def about(request):
