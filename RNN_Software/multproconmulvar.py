3eee5a44640c8a03e8d8624e440fcf79a80e480f
wagonsel <wagonsel@unlv.nevada.edu>
2019-11-25 12:31:14 -0800

    working on accuracy with mult vars

--- RNN_Software/multproconmulvar.py
+++ RNN_Software/multproconmulvar.py
@@ -2,22 +2,26 @@ from multiprocessing import Process, Lock, Array
 import os
 import time
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 global x
-x = 10
+x = 0
 
 start_time = time.time()
-def f(l, i, a):
-    while(a[x] < 20):
+def f(l, i, a,x):
+    while(a[x] <  19):
         l.acquire()
         a[x] = a[x]+1
+        temp = "./MultiVarTestOuput/Multivarsub/" + str(a[x]) + '.txt'
+        print(a[x])
         l.release()
-        os.system('python ./rnnmultvar.py ' + str(a[x]))
+        open(temp, 'w+')
+        os.system('python ./rnnmultvar.py' + ' ' + str(a[x] - 1) + ' > ' + str(temp) )
 
 if __name__ == '__main__':
     lock = Lock()
-    arr = Array('i', range(20))
+    arr = Array('i', range(19))
 
-    for num in range(9):
-        Process(target=f, args=(lock, num, arr)).start()
+    for num in range(10):
+        Process(target=f, args=(lock, num, arr,x)).start()
 
     print("--- %s seconds ---" % (time.time() - start_time))
