2517d4ba0f32a461d0a076b25b9bc352e8a71101
Nhung Barish <barisn1@unlv.nevada.edu>
2019-12-01 02:02:33 +0000

    Public Home page RSS and last 30 Days Chart

--- User_Interface_Django/aistockpicker/aistockpicker/views.py
+++ User_Interface_Django/aistockpicker/aistockpicker/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render_to_response
 from django.http import JsonResponse
 from urllib import request
 import urllib
-import json
-import requests
 from datetime import datetime, timedelta
+from datetime import datetime
+import requests
+import json
+import feedparser
 
 def handler404(request):
     return render(request, '404.html', status=404)
@@ -14,7 +16,26 @@ def handler404(request):
 def handler500(request):
     return render(request, '500.html', status=500)
 
+
+def plotly_scatter(df, _x):
+    x = df.iloc[:, 1:2]
+    y = df.iloc[:, 3:4]
+
+    trace1 = go.Scatter(x=x, y=y, marker={'color': 'red', 'symbol': 104, 'size': 10},
+                        mode="lines",  name='1st Trace')
+
+    data=go.Data([trace1])
+    layout=go.Layout(title="Meine Daten", xaxis={'title':'x1'}, yaxis={'title':'x2'})
+    figure=go.Figure(data=data,layout=layout)
+    div = py.plot(figure, auto_open=False, output_type='div')
+
+    return div
+
 def home(request):
+
+    url = 'https://www.nasdaq.com/feed/rssoutbound?category=Artificial+Intelligence'
+    rssdata = feedparser.parse(url)
+
     today = datetime.today()
     date_N_days_ago = datetime.now() - timedelta(days=30)
 
@@ -34,15 +55,18 @@ def home(request):
     current_date = datetime.now().strftime('%Y-%m-%d')
     start_date = date_N_days_ago.strftime('%Y-%m-%d')
 
-    #2019-10-28
-#    startDate = last_year +
+
     historical_response = requests.get('https://financialmodelingprep.com/api/v3/historical-price-full/GOOG?from=2019-10-28&to=2019-11-28')
     historical_stockdata = historical_response.json()
 
+
+
     #return JsonResponse({'mystring':"return this string"})
+
     return render(request,'accounts/home.html', {'authenticated': False,
                                                  'historical_stockdata': historical_stockdata,
                                                  'current_date': current_date,
+                                                 'rssdata': rssdata,
                                                  'start_date': start_date,
                                                  'last_month_text': last_month_text,
                                                  'last_year_full': last_year_full,
