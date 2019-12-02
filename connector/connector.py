90e61b98a32458fea9c1434898d4845f7989b80f
Bradyn Mensch <Mensch97@users.noreply.github.com>
2019-12-02 10:02:20 -0800

    read from .csv and insert into database

--- connector/connector.py
+++ connector/connector.py
@@ -1,16 +1,25 @@
+import csv
 import mysql.connector
 
 
 connection = mysql.connector.connect( host='unlvteamseven.mysql.pythonanywhere-services.com', user='unlvteamseven', passwd='Team7MySQL', db='unlvteamseven$aistockpickerdb' )
 cursor = connection.cursor()
 
-add_stocks = ("INSERT INTO stockData (id, timestamp, close, predicted) "
-                "VALUES (%s, %s, %s, %s)"
-                )
-
-data = ('google', '2019-11-27', 1312.13, 1310.33);
-
-cursor.execute(add_stocks, data)
+cursor.execute("DELETE FROM stockData")
 connection.commit()
 
-connection.close()
+with open('test.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+        else:
+            add_stocks = ("INSERT INTO stockData (id, timestamp, close, predicted) "
+                "VALUES (%s, %s, %s, %s)"
+                )
+            data = (row[0], row[1], row[2], row[3])
+            cursor.execute(add_stocks, data)
+            connection.commit()
+            line_count += 1
+    print(f'Processed {line_count} lines.')
