b9aec0fe5e4d5f677883383410dba34e87101cdc
Bradyn Mensch <Mensch97@users.noreply.github.com>
2019-12-02 09:59:28 -0800

    Revert "Merge branch 'master' of https://bitbucket.org/stefika/seniorprojectfall2019team7"
    This reverts commit 73cd8f8ad8da148665e091147cf34c284b9faa33, reversing
    changes made to ae603d4d73042d323c9db73fb8618cddeb46ecd7.

--- RNN_Software/accuracy_stats.py
+++ RNN_Software/accuracy_stats.py
@@ -1,6 +1,6 @@
 
 
-def calculate_accuracy(predicted_stock_price, real_stock_price, testname):
+def calculate_accuracy(predicted_stock_price, real_stock_price):
     # Calculating accuracy
     predicted_length = len(predicted_stock_price)
     # trend up == 1; trend down == 0; no change == 2
@@ -30,4 +30,4 @@ def calculate_accuracy(predicted_stock_price, real_stock_price, testname):
         total += 1
 
     percent_accurate = (accurate/total) * 100
-    print("Accuracy: " + str(percent_accurate) + "%" + testname)
+    print("Accuracy: " + str(percent_accurate) + "%")
--- RNN_Software/multproconmulvar.py
+++ RNN_Software/multproconmulvar.py
@@ -2,26 +2,22 @@ from multiprocessing import Process, Lock, Array
 import os
 import time
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 global x
-x = 0
+x = 10
 
 start_time = time.time()
-def f(l, i, a,x):
-    while(a[x] <  10):
+def f(l, i, a):
+    while(a[x] < 20):
         l.acquire()
         a[x] = a[x]+1
-        temp = "./MultiVarTestOuput/BestSub2/" + str(a[x]) + '.txt'
-        print(a[x])
         l.release()
-        open(temp, 'w+')
-        os.system('python ./rnnmultvar.py' + ' ' + str(a[x] - 1) + ' > ' + str(temp) )
+        os.system('python ./rnnmultvar.py ' + str(a[x]))
 
 if __name__ == '__main__':
     lock = Lock()
-    arr = Array('i', range(11))
+    arr = Array('i', range(20))
 
-    for num in range(11):
-        Process(target=f, args=(lock, num, arr,x)).start()
+    for num in range(9):
+        Process(target=f, args=(lock, num, arr)).start()
 
-print("--- %s seconds ---" % (time.time() - start_time))
+    print("--- %s seconds ---" % (time.time() - start_time))
