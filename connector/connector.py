16b139a5a4288770b41185f94f8c8ff965f7e433
Bradyn Mensch <Mensch97@users.noreply.github.com>
2019-12-02 13:22:40 -0800

    Getting the .csv files from Kevin into the database.

--- connector/connector.py
+++ connector/connector.py
@@ -2,24 +2,44 @@ import csv
 import mysql.connector
 
 
+Stocks = {
+"Amazon.com Inc",
+"Apple Inc",
+"Biogen Inc",
+"Booking Holdings Inc",
+"Celgene Corp",
+"Cisco Systems Inc",
+"Costco Wholesale Corp",
+"Microsoft Corp",
+"NVIDIA Corp",
+"eBay Inc"
+}
+
 connection = mysql.connector.connect( host='unlvteamseven.mysql.pythonanywhere-services.com', user='unlvteamseven', passwd='Team7MySQL', db='unlvteamseven$aistockpickerdb' )
 cursor = connection.cursor()
 
-cursor.execute("DELETE FROM stockData")
+cursor.execute("DELETE FROM stockpicker_predictions")
 connection.commit()
 
-with open('test.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            line_count += 1
-        else:
-            add_stocks = ("INSERT INTO stockData (id, timestamp, close, predicted) "
-                "VALUES (%s, %s, %s, %s)"
-                )
-            data = (row[0], row[1], row[2], row[3])
-            cursor.execute(add_stocks, data)
-            connection.commit()
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+idNo = 1
+
+for file in Stocks:
+    with open('/home/unlvteamseven/aistockpicker/static/predictions/' + file + '.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0 or line_count % 2 == 1:
+                line_count += 1
+            else:
+                add_stocks = ("INSERT INTO stockpicker_predictions (id, stockname, timestamp, close, prediction) "
+                    "VALUES (%s, %s, %s, %s, %s)"
+                    )
+                data = (idNo, row[0], row[1][0:4] + ',' + row[1][5:7] + ',' + row[1][8:], row[2], row[3])
+                cursor.execute(add_stocks, data)
+                connection.commit()
+                line_count += 1
+                idNo += 1
+                print(idNo)
+        print(f'Processed {line_count} lines.')
+
+connection.close()
